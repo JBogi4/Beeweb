e5caa0e62194dce537b8c202fd46ac34ba7f7ff3
JBogi4 <146484101+JBogi4@users.noreply.github.com>
2023-11-16 14:15:36 +0100

    Add files via upload

---
+++ web2/words.js
@@ -0,0 +1,53 @@
+const words = [
+
+{
+    word: "bee",
+    hint: "tiny yellow and black striped beetle, flying from flower to flower"
+},
+{
+    word: "honey",
+    hint: "sweet sticky syrup , also used in tea"
+},
+{
+    word: "honeydropper",
+    hint: "a special device for dispensing a sticky syrup"
+},
+{
+    word: "honeycomb",
+    hint: "the bees put the honey in it, hexagonal"
+},
+{
+    word: "beekeeper",
+    hint: "it is a human occupation, he deals with the black and yellow striped bugs"
+},
+{
+    word: "beehive",
+    hint: "this is the house of bees"
+},
+{
+    word: "beeswax",
+    hint: "a naturally occurring thing produced by yellow and black bugs in their house "
+},
+{
+    word: "flower",
+    hint: "the collective name for plants that do not have only green parts"
+},
+{
+    word: "wasp",
+    hint: "these bugs often have to be written out of houses, sting and resemble bees"
+},
+{
+    word: "lilac",
+    hint: "this is a purple flowering shrub, the flowers are in clusters"
+},
+{
+    word: "acacia",
+    hint: "it's a kind of tree , it's a kind of honey , it has spines"
+},
+{
+    word: "daisy",
+    hint: "it is an iconic flower with small white petals and a yellow centre"
+},
+
+
+]
